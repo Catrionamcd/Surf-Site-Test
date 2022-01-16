3cd791b2b92ab32ecd35f6fa8d77c3c2a5ac15f2
Catrionamcd <catrionamcd@gmail.com>
2022-01-16 23:05:21 +0000

    started on detailed product page

--- products/views.py
+++ products/views.py
@@ -7,6 +7,7 @@ from django.db.models.functions import Lower
 from .models import Product, Category, SubCategory, ProductInventory, Brand, Colour
 from .forms import ProductForm
 from checkout.models import Order, OrderLineItem
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -66,10 +67,11 @@ def all_products(request):
         brand_checked = list(brands.values_list('id', flat=True))
         colour_checked = list(colours_list.values_list('id', flat=True))
 
-
-    # """ First get full product list and annotate the sale price of each item """
-    menu_query = Q(category__in=cat_checked) | Q(subcategory__in=sub_checked)
-    products = Product.objects.all().filter(menu_query)
+    # """ First get full product list and annotate the sale price of each item """                
+    products = Product.objects.all().exclude(obsolete=True).filter(
+        Q(category__in=cat_checked) | Q(subcategory__in=sub_checked),
+        Q(brand__in=brand_checked)
+    ).annotate(order_count=Count('orderlineitem'))
     # products = Product.objects.all().annotate(sale_price=F('price') / 100 * (100 - F('category__sale_percent')))
 
     query = None
@@ -108,12 +110,18 @@ def all_products(request):
 
     current_sorting = f'{sort}_{direction}'
 
+    product_count = products.count()
+    paginator = Paginator(products, 25)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
         'categories_list': categories_list,
         # 'subcategories_list': subcategories_list,
         'brands': brands,
         'colours': colours,
-        'products': products,
+        'products': page_obj,
+        'product_count': product_count,
         'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
@@ -121,10 +129,13 @@ def all_products(request):
         'cat_indeterminate': cat_indeterminate,
         'sub_checked': sub_checked,
         'brand_checked': brand_checked,
-        'colour_checked': colour_checked
+        'colour_checked': colour_checked,
+        'is_paginated': 8,
+        'paginate_by': 8
     }
-
+    paginate_by = 8
     return render(request, 'products/products.html', context)
+ 
 
 
 def product_detail(request, product_id):
@@ -134,11 +145,12 @@ def product_detail(request, product_id):
     """ Calculate product sale price which will be used if sale in progress """
     # sale_price = product.price / 100 * (100 - product.category.sale_percent)
 
+
     """ Get Product that was ordered the most while ordering this selected product """
     """ First get a list of all Orders that include this selected Product """
     orders_with_this_product = OrderLineItem.objects.filter(product=product_id).values_list('order',flat=True)
     """ Next get a list of Product Items ordered in all of the Orders retrieved above BUT not giftcards """
-    all_products_in_these_orders = OrderLineItem.objects.filter(order__in=orders_with_this_product).exclude(product__category__giftcard_category=True)
+    all_products_in_these_orders = OrderLineItem.objects.filter(order__in=orders_with_this_product).exclude(product__category__giftcard_category=True).exclude(product__obsolete=True)
     """ Now Count how many times each product was ordered across all of these Orders """
     each_product_count = all_products_in_these_orders.values('product').order_by('product').annotate(num_ordered=Count('product'))
     """ Finally sequence from largest to smallest count """
@@ -161,6 +173,12 @@ def product_detail(request, product_id):
                 freq_bought_together_id = each_product_count_desc[1]['product']
                 freq_bought_together = get_object_or_404(Product, pk=freq_bought_together_id)
 
+    """ if orders found for this product hide Admin product delete option in product page """ 
+    product_has_no_orders = True
+    if orders_with_this_product:
+        print("HAS ORDERS")
+        product_has_no_orders = False
+
     # print(len(each_product_count_desc))
     # print(each_product_count_desc[0]['product'], each_product_count_desc[0]['num_ordered'])
 
@@ -173,6 +191,7 @@ def product_detail(request, product_id):
         'product': product,
         # 'sale_price': sale_price,
         'freq_bought_together': freq_bought_together,
+        'product_has_no_orders': product_has_no_orders,
     }
 
     return render(request, 'products/product_detail.html', context)
@@ -230,6 +249,7 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
 
 @login_required
 def delete_product(request, product_id):
